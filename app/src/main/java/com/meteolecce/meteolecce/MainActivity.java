b6304f396464e3ea21bc260d8bfee34de7c55d9b
remotomasi <remo.tomasi@gmail.com>
2017-09-07 18:50:35 +0200

    Initial commit

---
+++ app/src/main/java/com/meteolecce/meteolecce/MainActivity.java
@@ -0,0 +1,91 @@
+package com.meteolecce.meteolecce;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import java.io.IOException;
+import org.json.JSONObject;
+import org.apache.http.HttpResponse;
+//import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+//import org.apache.http.client.methods.HttpGet;
+//import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.client.ClientProtocolException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt = null;
+    String module = null;
+    final String site = "http://api.openweathermap.org/data/2.5/weather?q=Lecce,it&appid=35222ccfcb5285d12e8a0e3222d59d9c";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new readWeatherLecce().execute();
+
+    }
+
+    private class readWeatherLecce extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            txt = (TextView) findViewById(R.id.textView7);
+
+            String str = "";
+            HttpResponse response;
+            DefaultHttpClient myClient = new DefaultHttpClient();
+            //HttpClient myClient = HttpClientBuilder.create().build();
+            HttpPost myConnection = new HttpPost(site);
+
+            try {
+                response = myClient.execute(myConnection);
+                str = EntityUtils.toString(response.getEntity(), "UTF-8");
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                JSONObject json = new JSONObject(str);
+                module = json.getJSONObject("main").getString("temp");
+                //txt.setText(module);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+            txt.setText(module);
+            super.onPostExecute(result);
+        }
+    }
+
+
+
+    private abstract class GetContacts extends AsyncTask<Void, Void, Void> {
+        //public void readWeatherLecce() {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this, "Json Data is downloading", Toast.LENGTH_LONG).show();
+
+
+
+        }
+
+        //}
+
+    }
+}
